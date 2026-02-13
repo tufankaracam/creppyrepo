6e8a8985c3959b2f834c6fb99e11d25857059526
tufankaracam <tufankaracam@hotmail.com.tr>
2026-02-13 18:13:28 +0100

    super comm

---
+++ src/helper.js
@@ -0,0 +1,13 @@
+const sum = (x,y)=>{
+    return x+y;
+}
+
+const multiply = (x,y)=>{
+    return x*y;
+}
+
+const substract = (x,y)=>{
+    return x-y;
+}
+
+module.exports = {sum,multiply,substract}
